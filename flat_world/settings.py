8eaaa4f76d7c7a0293a9384b8af3c0a93d6a97e2
Bee <quakashaka@gmail.com>
2022-04-26 15:13:56 +0400

    db fix

--- flat_world/settings.py
+++ flat_world/settings.py
@@ -86,9 +86,10 @@ WSGI_APPLICATION = 'flat_world.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
